eec996ca83dbc74767e3e06219e658e5681f3721
janesessive <eugene.absessiveabs@gmail.com>
2019-06-05 18:53:26 +0300

    Navigation

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable react/jsx-no-undef */
+import React from 'react';
+import Navigation from './components/Navigation/Navigation.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Navigation = () => {
+    return (
+        <nav>Sign Out</nav>
+    );
+}
+
+export default Navigation;
